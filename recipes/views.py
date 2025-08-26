cc9b9315b4da9e01397a84a0a5c7139cfc9adeb6
devguizao <guilherme.luizc13@gmail.com>
2025-08-26 14:45:24 -0300

    Exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Guilherme Luiz',
+    })
 
 def contato(request):
-    return HttpResponse('meu contato')
+    return render(request, 'me-apague/temp.html')
 
 def sobre(request):
     return HttpResponse('sobre mim')
