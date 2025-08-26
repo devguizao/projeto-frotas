050495ecd41af59f2cb12dd77a257ca5d64de939
devguizao <guilherme.luizc13@gmail.com>
2025-08-26 13:40:00 -0300

    movendo o codigo para o app de recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,26 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-# HTTP REQUEST <- HTTP RESPONSE
-
-# HTTP REQUEST
-def home(request):
-    return HttpResponse('home')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), #home
-    path('sobre/', sobre), #/sobre/
-    path('contato/', contato), #/contato/
+    path('', include('recipes.urls')),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('home 2')
+
+def contato(request):
+    return HttpResponse('meu contato')
+
+def sobre(request):
+    return HttpResponse('sobre mim')
