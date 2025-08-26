b73b268eb3cfcd24a05dec9b0a7cb757028c2cd1
devguizao <guilherme.luizc13@gmail.com>
2025-08-26 14:26:54 -0300

    templates e renderização de html no django

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home 2')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('meu contato')
